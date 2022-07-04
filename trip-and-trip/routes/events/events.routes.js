2f44be17209703a1de74cdb8f5bb90c6f72536a6
afisman <afisman@gmail.com>
2022-07-04 13:05:42 +0200

    created event creation form, is creating objects in database correctly, needs plants in database to be able to pick them, needs clodinary integration to be able to upload images

--- trip-and-trip/routes/events/events.routes.js
+++ trip-and-trip/routes/events/events.routes.js
@@ -1,15 +1,33 @@
 const router = require('express').Router()
 const Event = require('./../../models/Event.model')
+const Plant = require('./../../models/Plant.model')
+
 
 router.get('/events', (req, res, next) => {
     res.render('events/event-list')
 })
 
 router.get('/events/create', (req, res, next) => {
-    res.send('H')
+    Plant
+        .find()
+        .then(plants => {
+            console.log(plants)
+            res.render('events/event-create', { plants })
+        })
+        .catch((err) => console.log(err))
+
 })
 
 router.post('/events/create', (req, res, next) => {
+    // const { id } = req.session.currentUser._id
+    const { date, plants, description } = req.body
+
+    Event
+        .create({ date, plants, description })
+        .then(event => res.send(event))
+        .catch((err) => console.log(err))
+
+
 })
 
 router.get('/events/:id', (req, res, next) => {
