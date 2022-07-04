0fff829fa851008dda1c9133be399b0f55ab1ab1
rafasanzvi <rafasv41@gmail.com>
2022-07-04 14:01:28 +0200

    Form to create plants and routes of plants, plants and plants create routes created

--- trip-and-trip/routes/plant/plant.routes.js
+++ trip-and-trip/routes/plant/plant.routes.js
@@ -5,13 +5,27 @@ const Plant = require('./../../models/Plant.model')
 
 router.get('/plants', (req, res, next) => {
 
+    Plant
+        .find()
+        .then(plants => {
+            res.render('plant/plants-list', { plants })
+        })
+        .catch(err => console.log(err))
 })
 
 router.get('/plants/create', (req, res, next) => {
 
+    res.render('plant/new-plant')
 })
 
 router.post('/plants/create', (req, res, next) => {
+
+    const { scientificName, commonName, region, cultures, files, types, description } = req.body
+    const createPlant = req.body
+
+    Plant
+        .create()
+
 
 })
 
