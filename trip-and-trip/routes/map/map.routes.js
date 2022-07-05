872de01950a65f46c2cafc31995f59e972edee45
afisman <afisman@gmail.com>
2022-07-05 13:22:26 +0200

    fixed layout of plant details, adding image and listing correctly properties and regions

--- trip-and-trip/routes/map/map.routes.js
+++ trip-and-trip/routes/map/map.routes.js
@@ -1,5 +1,5 @@
-const router = require("express").Router();
+const router = require("express").Router()
 
 router.get("/basic", (req, res, next) => res.render("map/map-basic"))
 
-module.exports = router;
+module.exports = router
